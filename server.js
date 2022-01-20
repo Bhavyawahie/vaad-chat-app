9e1e19ac0743731e3fca1d49ce0b8edd0fcaf75c
Bhavya Wahie <bhavyawahie@gmail.com>
2022-01-21 04:23:23 +0530

    jwt added as dependency and server tweaked for middlewares and routes+controllers

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
+// const server = http.createServer(app)
+// const io = require('socket.io')(server, {})
 const express = require('express');
 const app = express()
+const http = require('http')
 const path = require('path')
 const dotenv = require('dotenv');
 const morgan = require('morgan')
-const colors = require('colors');
+require('colors');
+
+const connectDB = require('./config/db')
 
 const chats = require('./data/data')
 const chatRoutes = require('./routes/chatRoutes')
 
 dotenv.config({path: './.env'})
 
+connectDB()
+
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+app.use('/api/users', userRoutes)
 app.use('/api/chats', chatRoutes)
 
 const PORT = process.env.PORT || 4000
+
 app.listen(PORT, () => {
-    console.log(`Server started running at http://localhost:${PORT}`.green.inverse);
+    console.log(`Server started running at http://localhost:${PORT}`.yellow.inverse);
 })
