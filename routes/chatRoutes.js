ba101fe4f4732d2528b7ae20fc63972286dc5246
Bhavya Wahie <bhavyawahie@gmail.com>
2022-01-25 05:27:32 +0530

    chatRoutes added

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const { getChats, getSingleChat } = require('../controllers/chatController')
+const { protect } = require('../middlewares/authMiddleware')
 const router = express.Router()
 
-router.route('/').get(getChats)
-router.route('/:id').get(getSingleChat)
+router.route('/').post(protect, accessChat).get(protect, fetchChats)
+router.route('/group').post(protect, createGroupChat)
+router.route('/group/rename').put(protect, renameGroupChat)
+router.route('/group/remove').put(protect, removeFromGroupChat)
+router.route('/group/add').put(protect, addToGroupChat)
 
 module.exports = router
