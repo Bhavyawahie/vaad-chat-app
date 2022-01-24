4e49d5887feeb1c46d542d5155ebfc3a6a340a25
Bhavya Wahie <bhavyawahie@gmail.com>
2022-01-24 08:06:27 +0530

    errors corrected in the middlewares and made to work

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
-const { Error } = require('mongoose')
 
 const protect = asyncHandler(async (req, res, next) => {
     let token
-    if(req.headers.authorization && req.headers.authentication.startsWith('Bearer')){
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
         try {
             token = req.headers.authorization.split(' ')[1]
             const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-            req.user = await User.findById(decodedToken).select('-password')
+            req.user = await User.findById(decodedToken.id).select('-password')
             next()
         } catch (error) {
+            console.error(error)
             res.status(401)
             throw new Error('Not Authorized, token is invalid!')
         }
