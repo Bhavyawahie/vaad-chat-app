aeb3410ba9646d89958409e037ccc9db8f2673f0
Bhavya Wahie <bhavyawahie@gmail.com>
2022-02-14 23:05:28 +0530

    sidebar profile contents updated

--- client/src/components/SidebarProfile.jsx
+++ client/src/components/SidebarProfile.jsx
@@ -1,20 +1,35 @@
-import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, Input } from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { Avatar, Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, Flex, Input, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Sidebarprofile = ({isOpen, onClose, btnRef}) => {
+const Sidebarprofile = ({isOpen, onClose}) => {
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
     return (
         <Drawer
             isOpen={isOpen}
             placement='left'
             onClose={onClose}
-            finalFocusRef={btnRef}
             size='sm'
             >
             <DrawerContent shadow="none" w='25%' maxW='25%'>
-                <DrawerCloseButton/>
-                <DrawerHeader>Create your account</DrawerHeader>
+            <DrawerHeader>
+                    <Flex>
+                        <Button variant="flushed" onClick={onClose} _focus={{outline: "none"}}>
+                            <ArrowBackIcon/>
+                        </Button>
+                        <Text p={1}>Profile</Text>
+                    </Flex>
+                </DrawerHeader>
                 <DrawerBody>
-                    <Input placeholder='Type here...' />
+                    <Flex flexDir='column' alignItems='center' justifyContent='center'>
+                        <Box my={8}>
+                            <Avatar name={userInfo.name} src={userInfo.displayPicture} as='Input' type='file' size='2xl'/>
+                        </Box>
+                        <Text>{userInfo.name}</Text>
+                    </Flex>
                 </DrawerBody>
                 <DrawerFooter>
                     <Button variant='outline' mr={3} onClick={onClose}>Cancel</Button>
