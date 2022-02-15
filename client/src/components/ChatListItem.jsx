fa05a3703d8c7b7de020e5a1be55f3c3a892e7e6
Bhavya Wahie <bhavyawahie@gmail.com>
2022-02-16 02:18:06 +0530

    chatlistitem component added

--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const ChatListItem = ({id, name, displayPicture, lastMessage, initiateChat}) => {
     return (
-        <Flex w='100%' px={3} py={.5} flexDirection='row' borderRadius='10px' _hover={{backgroundColor: "#EDF2F7"}} onClick={() => initiateChat(id)}>
-                <Box py={1} mr={4}>
-                    <Avatar name={name} src={displayPicture} backgroundColor="#DADADA"/>
+        <Flex w='100%' px={3} py={1} flexDirection='row' borderRadius='10px' _hover={{backgroundColor: "#EDF2F7"}} onClick={() => initiateChat(id)}>
+                <Box mr={4} ml={1}>
+                    <Avatar name={name} src={displayPicture} backgroundColor="#DADADA" />
                 </Box>
-                <Box>
-                    <Text>{name}</Text>
-                    <Text>{lastMessage}</Text>
+                <Box as='Flex' justifyContent='center'>
+                    <Text fontSize="lg">{name}</Text>
+                    <Text fontSize="sm" color="blackAlpha.600">{lastMessage}</Text>
                 </Box>
             </Flex>
     )
