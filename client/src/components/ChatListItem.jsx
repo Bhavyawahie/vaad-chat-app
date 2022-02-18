26afc6493f01879d7a5b2d549b49d0ffe47af36b
Bhavya Wahie <bhavyawahie@gmail.com>
2022-02-18 18:53:30 +0530

    timestamps added to chatlistitem

--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -1,14 +1,18 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Spacer, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const ChatListItem = ({id, name, displayPicture, lastMessage, initiateChat}) => {
+const ChatListItem = ({id, name, displayPicture, lastMessage, timeStamp, initiateChat}) => {
     return (
         <Flex w='100%' px={3} py={1} flexDirection='row' borderRadius='10px' _hover={{backgroundColor: "#EDF2F7"}} onClick={() => initiateChat(id)}>
                 <Box mr={4} ml={1}>
                     <Avatar name={name} src={displayPicture} backgroundColor="#DADADA" />
                 </Box>
-                <Box as='Flex' justifyContent='center'>
-                    <Text fontSize="lg">{name}</Text>
+                <Box as='Flex' justifyContent='center' width='500px'>
+                    <Flex justifyContent="space-between">
+                        <Box fontSize="lg">{name}</Box>
+                        <Spacer/>
+                        <Box fontSize="xs" color="blackAlpha.400">{timeStamp.split('.')[0]}</Box>
+                    </Flex>
                     <Text fontSize="sm" color="blackAlpha.600">{lastMessage}</Text>
                 </Box>
             </Flex>
