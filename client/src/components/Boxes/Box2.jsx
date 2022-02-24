fb6766a5b176be41702d0a983ada26a8146597bf
Bhavya Wahie <bhavyawahie@gmail.com>
2022-02-25 01:50:21 +0530

    useeffect now triggered on messageSent

--- client/src/components/Boxes/Box2.jsx
+++ client/src/components/Boxes/Box2.jsx
@@ -3,16 +3,21 @@ import { useSelector, useDispatch } from 'react-redux'
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon'
 import { Avatar, Box, Button, Flex, Icon, Input, Text } from '@chakra-ui/react'
 import { fetchAllMessages, sendMessage } from '../../actions/messageActions'
+import MessageScrollList from '../MessageScrollList'
+import { getReciever } from '../../utils/chatLogics'
 
 const Box2 = ({setSideBox}) => {
+    const groupChatImgURL = `https://nirc.icai.org/wp-content/plugins/profilegrid-user-profiles-groups-and-communities/public/partials/images/default-group.png`
     const [messageField, setMessageField] = useState("")
     const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
     const messageSend = useSelector(state => state.messageSend)
-    const {loading, message, error} = messageSend
+    const { messages: messageSent, error} = messageSend
+    const messageListAll = useSelector(state => state.messageListAll)
+    const {loading: loadingMessage, error: errorMessage } = messageListAll
     const chatCurrentSet = useSelector(state => state.chatCurrentSet)
     const {currentChat} = chatCurrentSet
-    const messageListAll = useSelector(state => state.messageListAll)
-    const {loading: loadingMessage, messages, error: errorMessage } = messageListAll
     const sendMessageHandler = () => {
         dispatch(sendMessage(currentChat._id, messageField))
         setMessageField("")
@@ -21,8 +26,7 @@ const Box2 = ({setSideBox}) => {
         if(currentChat){
             dispatch(fetchAllMessages(currentChat._id))
         }
-    }, [currentChat])
-    console.log(messages)
+    }, [currentChat, messageSent])
     return (
         <Box bg="white" w="75%" minWidth="50%" borderLeft="1px solid rgb(229,229,229)">
                 { currentChat ? (
@@ -30,12 +34,16 @@ const Box2 = ({setSideBox}) => {
                     <Flex height='9%' flexDirection="row" justifyContent="flex-start" borderBottom='1px solid rgb(229,229,229)' py={3}>
                             <Flex w='80%' ml={4} onClick={() => setSideBox(true) }>
                                 <Box>
-                                    <Avatar src={currentChat.users[0].displayPicture} boxSize="40px" borderRadius="50%"></Avatar>
+                                    <Avatar src={currentChat.isGroupChat ? groupChatImgURL : getReciever(userInfo, currentChat.users).displayPicture} boxSize="2.5rem" borderRadius="50%"></Avatar>
                                 </Box>
-                                <Text fontSize='lg' ml={3}>{currentChat.chatName}</Text>
+                                <Text fontSize='lg' ml={3}>{  currentChat.isGroupChat ? currentChat.chatName : getReciever(userInfo, currentChat.users).name}</Text>
                             </Flex>
                     </Flex>
-                    <Flex height='83.4%' justifyContent='center' alignItems='center'>BC</Flex>
+                    <Flex height='83.4%' w='100%'>
+                        <Flex w="100%" h="100%" justifyContent='center' alignItems='center' flexDirection='column' alignItems='flex-end' overflowY="hidden">
+                            {loadingMessage ? <span>loading</span> : <MessageScrollList/>}
+                        </Flex>
+                    </Flex>
                     <Flex height='7.6%' px={2}>
                         <Button variant='flushed' _hover={{backgroundColor: "rgba(229,229,229)"}}><Icon as={InsertEmoticonIcon}/></Button>
                         <Input placeholder="Type a Message" borderRadius='25px' w='90%' value={messageField} onChange={(e) => setMessageField(e.target.value)} onKeyPress={(e) => (e.key === 'Enter') && sendMessageHandler()}/>
