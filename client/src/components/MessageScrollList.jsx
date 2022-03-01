0d6c24dbec2e3f39de9f518adba21cf20bde257e
Bhavya Wahie <bhavyawahie@gmail.com>
2022-03-01 21:22:20 +0530

    messages layed with proper styles and timestamps

--- client/src/components/MessageScrollList.jsx
+++ client/src/components/MessageScrollList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Flex, Spacer } from '@chakra-ui/react'
 import ScrollableFeed from 'react-scrollable-feed'
 import { isSameSenderMargin } from '../utils/chatLogics'
 
@@ -12,7 +12,14 @@ const MessageScrollList = () => {
     return (
         <ScrollableFeed className='messageScrollList'>
             {messages && messages.map((message, index) => (
-                <Box width='max-content' key={message._id} d='flex' backgroundColor={message.sender._id === userInfo.id ? "#BEE3F8" : "#B9F5D0"} justifyContent={message.sender._id === userInfo.id ? `flex-end`: `flex-start`} py={2} px={4} my={4} borderRadius='20px' marginLeft={isSameSenderMargin(messages, message, index, userInfo.id)}>{message.content}</Box>
+                <Flex width='max-content' key={message._id} backgroundColor={`#EDF2F7`} py={2} px={4} my={4} borderRadius='20px' marginLeft={isSameSenderMargin(messages, message, index, userInfo.id)}>
+                    <Box>
+                        {message.content}
+                    </Box>
+                    <Box d='flex' fontSize={{ base: '6px', md: '9px', lg: '11px' }} alignItems='flex-end' pt={2} ml={3} color='blackAlpha.400'>
+                        {message.createdAt.split('T')[1].slice(0, -8)}
+                    </Box>
+                </Flex>
             ))}
         </ScrollableFeed>
     )
