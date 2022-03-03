e69bec64a0e2f33888838307062053cf5e26e52a
Bhavya Wahie <bhavyawahie@gmail.com>
2022-03-03 20:00:52 +0530

    profile pic input, input-label, icon and styles added

--- client/src/components/SidebarProfile.jsx
+++ client/src/components/SidebarProfile.jsx
@@ -1,7 +1,8 @@
 import { ArrowBackIcon } from '@chakra-ui/icons';
-import { Avatar, Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, Flex, Input, Text } from '@chakra-ui/react';
+import { Avatar, Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, Flex, IconButton, Input, Text } from '@chakra-ui/react';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'
+import PhotoCamera from '@mui/icons-material/PhotoCamera'
 
 const Sidebarprofile = ({isOpen, onClose}) => {
     const dispatch = useDispatch()
@@ -25,8 +26,14 @@ const Sidebarprofile = ({isOpen, onClose}) => {
                 </DrawerHeader>
                 <DrawerBody>
                     <Flex flexDir='column' alignItems='center' justifyContent='center'>
-                        <Box my={8}>
-                            <Avatar name={userInfo.name} src={userInfo.displayPicture} as='Input' type='file' size='2xl'/>
+                        <Box my={8} position='relative'>
+                            <Avatar name={userInfo.name} src={userInfo.displayPicture} size='2xl'/>
+                                <label for="icon-button-file" className='icon-button-file-label'>
+                                    <Input id="icon-button-file" type="file" accept='image/*'/>
+                                    <IconButton aria-label="upload picture" isRound variant='ghost' width='8rem' height='8rem'>
+                                        <PhotoCamera />
+                                    </IconButton>
+                                </label>
                         </Box>
                         <Text>{userInfo.name}</Text>
                     </Flex>
