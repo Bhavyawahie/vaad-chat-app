7a35c1eea0c3599ebd3672e0ba4c9d34e7ec6a98
Bhavya Wahie <bhavyawahie@gmail.com>
2022-03-09 22:24:13 +0530

    chatlistitem, chatlist update & created

--- client/src/components/ChatListItem.jsx
+++ client/src/components/ChatListItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Avatar, Box, Flex, Spacer, Text } from '@chakra-ui/react'
 
-const ChatListItem = ({id, name, displayPicture, lastMessage, timeStamp, initiateChat}) => {
+const ChatListItem = ({id, name, displayPicture, lastMessage, timeStamp, initiateChat, openContext}) => {
     return (
-        <Flex w='100%' px={3} py={1} flexDirection='row' borderRadius='10px' _hover={{backgroundColor: "#EDF2F7"}} onClick={() => initiateChat(id)}>
+        <Flex w='100%' px={3} py={1} flexDirection='row' borderRadius='10px' _hover={{backgroundColor: "#EDF2F7"}} onClick={() => initiateChat(id)} onContextMenu={() => openContext(id)}>
                 <Box mr={4} ml={1}>
                     <Avatar name={name} src={displayPicture} backgroundColor="#DADADA" />
                 </Box>
