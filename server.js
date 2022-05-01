ed1341dc1d484030186a60e349c3a2325dc2596d
Bhavya Wahie <bhavyawahie@gmail.com>
2022-05-01 22:13:53 +0530

    express-fileupload and cloudinary added as dependencies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const http = require('http')
 const path = require('path')
+const fileUpload = require('express-fileupload')
 const dotenv = require('dotenv');
 const morgan = require('morgan')
 const server = http.createServer(app)
@@ -25,8 +26,9 @@ dotenv.config({path: './.env'})
 
 connectDB()
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({limit: '50mb', extended: true}))
+app.use(express.json({limit: '50mb'}))
+app.use(fileUpload())
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
@@ -45,7 +47,7 @@ server.listen(PORT, () => {
 })
 
 io.on('connection', (socket) => {
-    console.log("connected to the socket")
+    console.log("Connected to the socket".red.inverse)
 
 
     socket.on('message', (message) => {
