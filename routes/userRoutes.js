ae2b32a47173556615f8a759bda19313879156c5
Bhavya Wahie <bhavyawahie@gmail.com>
2022-05-01 23:17:52 +0530

    new route for displayPic updation added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const {protect} = require('../middlewares/authMiddleware')
-const { register, authUser, allUsers } = require('../controllers/userController')
+const { register, authUser, allUsers, changeDisplayPicture } = require('../controllers/userController')
 const router = express.Router()
 
 router.route('/').get(protect, allUsers).post(register)
 router.route('/login').post(authUser)
+router.route('/:userId').patch(protect, changeDisplayPicture)
 module.exports = router
