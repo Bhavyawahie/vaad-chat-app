caab8a712e7e62e3221268ea12b78d83ad7c9080
Bhavya Wahie <bhavyawahie@gmail.com>
2022-05-02 00:21:08 +0530

    profile pic update loader added

--- client/src/components/SidebarProfile.jsx
+++ client/src/components/SidebarProfile.jsx
@@ -1,15 +1,18 @@
-import { ArrowBackIcon } from '@chakra-ui/icons';
-import { Avatar, Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, Flex, IconButton, Input, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux'
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { Avatar, Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, Flex, IconButton, Input, Spinner,Text } from '@chakra-ui/react';
 import PhotoCamera from '@mui/icons-material/PhotoCamera'
+import {updateDisplayPicture} from '../actions/userActions'
 
 const Sidebarprofile = ({isOpen, onClose}) => {
     const dispatch = useDispatch()
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
+    const userDisplayPictureUpdate = useSelector(state => state.userDisplayPictureUpdate)
+    const {loading, success, error} = userDisplayPictureUpdate
     const uploadImageHandler = (picture) => {
-        // dispatch(updateDisplayPicture(picture))
+        dispatch(updateDisplayPicture(picture))
     }
     return (
         <Drawer
@@ -30,21 +33,20 @@ const Sidebarprofile = ({isOpen, onClose}) => {
                 <DrawerBody>
                     <Flex flexDir='column' alignItems='center' justifyContent='center'>
                         <Box my={8} position='relative'>
-                            <Avatar name={userInfo.name} src={userInfo.displayPicture} size='2xl'/>
-                            <label for="icon-button-file" className='icon-button-file-label'>
-                                <input id="icon-button-file" type='file' accept='image/*' onChange={(e) => uploadImageHandler(e.target.files[0]) } />
-                                <Box>
-                                    <PhotoCamera />
-                                </Box>
-                            </label>
+                            {loading ? <Flex w='8rem' h='8rem' justifyContent='center' alignItems='center'><Spinner thickness='4px'speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl'/></Flex> :(
+                                <>
+                                    <Avatar name={userInfo.name} src={userInfo.displayPicture} size='2xl'/>
+                                    <label for="icon-button-file" className='icon-button-file-label'>
+                                    <input id="icon-button-file" type='file' accept='image/*' onChange={(e) => uploadImageHandler(e.target.files[0]) } />
+                                    <Box>
+                                        <PhotoCamera />
+                                    </Box>
+                                    </label>
+                                </>)}
                         </Box>
                         <Box>{userInfo.name}</Box>
                     </Flex>
                 </DrawerBody>
-                <DrawerFooter>
-                    <Button variant='outline' mr={3} onClick={onClose}>Cancel</Button>
-                    <Button colorScheme='blue'>Save</Button>
-                </DrawerFooter>
             </DrawerContent>
         </Drawer>
     )
