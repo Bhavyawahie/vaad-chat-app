b9ff2b7c20d425bff7da36656ef5f7e8cf78bb86
Bhavya Wahie <bhavyawahie@gmail.com>
2022-05-03 02:45:22 +0530

    box3 for group and single chats separated using conditional rendering

--- client/src/components/Boxes/Box3.jsx
+++ client/src/components/Boxes/Box3.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { Avatar, Box, Button, DrawerHeader, Flex, Heading, IconButton,Text } from '@chakra-ui/react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { CloseIcon } from '@chakra-ui/icons'
+import { getReciever } from '../../utils/chatLogics'
+import groupIcon from '../../img/groupIcon.png'
+import UserListItem from '../UserListItem'
+import { MESSAGE_ALL_LIST_RESET } from '../../constants/messageConstants'
+import { CHAT_CURRENT_RESET, CHAT_CURRENT_SET } from '../../constants/chatConstants'
 
 const Box3 = ({setSideBox}) => {
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
     const chatCurrentSet = useSelector(state => state.chatCurrentSet)
     const { currentChat } = chatCurrentSet
+    const chatAllList = useSelector(state => state.chatAllList)
+    const {chats} = chatAllList
+    const chatOpener = (chatId) => {
+        setSideBox(false)
+        if(currentChat && chatId !== currentChat._id){
+            dispatch({type: MESSAGE_ALL_LIST_RESET})
+        }
+        dispatch({type: CHAT_CURRENT_RESET})
+        dispatch({type: CHAT_CURRENT_SET, payload: chats.find(chat => chat._id === chatId)})
+    }
     return (
         <Box bg="white" w='25%' minWidth="25%" borderLeft="1px solid rgb(229,229,229)">
             <Flex justifyContent='center' alignItems='center' flexDirection='column' w='100%'>
@@ -13,11 +31,27 @@ const Box3 = ({setSideBox}) => {
                     <Box>
                         <IconButton size='sm' variant='ghost' onClick={() => setSideBox(false)} icon={<CloseIcon opacity='80%'/>}/>
                     </Box>
-                    <Text fontSize='xl' fontWeight='semibold' ml={3}>Contact Info</Text>
+                    <Text fontSize='xl' fontWeight='semibold' ml={3}>{currentChat.isGroupChat ? 'Group Chat Info' : 'Contact Info'}</Text>
                 </Flex>
-                <Avatar size='2xl' mt={6}></Avatar>
-                <Text as='h3'>Name</Text>
-                <Text>exmaple@gmail.com</Text>
+                {currentChat.isGroupChat ? (
+                    <Flex justifyContent='center' alignItems='center' flexDirection='column' w='100%' px={2}>
+                        <Avatar size='2xl' mt={6} src={groupIcon}/>
+                        <Text as='h3' fontSize='2xl' mt={2} mb={4}>{currentChat.chatName}</Text>
+                        {currentChat.users.map(user => (
+                            <UserListItem 
+                                key={user._id}
+                                user={user}
+                                initiateChat={chatOpener}
+                            />
+                        ))}
+                    </Flex>
+                    ): (
+                    <Flex justifyContent='center' alignItems='center' flexDirection='column'>
+                        <Avatar size='2xl' mt={6} src={getReciever(userInfo, currentChat.users).displayPicture}/>
+                        <Text as='h3' fontSize='2xl' my={2}>{getReciever(userInfo, currentChat.users).name}</Text>
+                        <Text fontSize='lg'>{getReciever(userInfo, currentChat.users).email}</Text>
+                    </Flex>
+                )}
             </Flex>
         </Box>
     )
