5d424fa1b84682634f0dbdc11b3ccc6a19893680
Bhavya Wahie <bhavyawahie@gmail.com>
2022-05-03 19:26:39 +0530

    staging for deployment

--- server.js
+++ server.js
@@ -40,6 +40,15 @@ app.use('/api/messages', messageRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+} else {
+    app.get("/", (req, res) => {
+        res.send("API in work!")
+    })
+}
+
 const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
