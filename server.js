daf784eb0d320732d78af37afb7f9ba84b157c03
Bhavya Wahie <bhavyawahie@gmail.com>
2022-05-25 23:04:12 +0530

    production check

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(errorHandler)
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join('client/build')))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+    app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 } else {
     app.get("/", (req, res) => {
         res.send("API in work!")
