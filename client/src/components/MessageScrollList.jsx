6b855d35fb1685b20df329fec76b2ef972b4594a
Bhavya Wahie <bhavyawahie@gmail.com>
2023-08-14 17:44:06 +0530

    skeleton renders for messages now in tandem with screen size

--- client/src/components/MessageScrollList.jsx
+++ client/src/components/MessageScrollList.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { Box, Flex } from '@chakra-ui/react'
 import ScrollableFeed from 'react-scrollable-feed'
 import { isSameSenderMargin } from '../utils/chatLogics'
+import { formatISODate } from '../utils/dateAndTimeFormater'
 
 const MessageScrollList = () => {
     const userLogin = useSelector(state => state.userLogin)
@@ -17,7 +18,7 @@ const MessageScrollList = () => {
                         {message.content}
                     </Box>
                     <Box d='flex' fontSize={{ base: '6px', md: '9px', lg: '11px' }} alignItems='flex-end' pt={2} ml={3} color='blackAlpha.400'>
-                        {message.createdAt.split('T')[1].slice(0, -8)}
+                        {formatISODate(message.createdAt)}
                     </Box>
                 </Flex>
             ))}
--- client/src/components/UserLoading.jsx
+++ client/src/components/UserLoading.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Skeleton, Stack } from '@chakra-ui/react';
 
 const Userloading = () => {
+    const [skeletonEl, setSkeletonEl] = useState([])
+    useEffect(() => {
+        setSkeletonEl([...Array(Math.floor(document.body.scrollHeight/40)).keys()])
+    }, [])
     return (
         <Container my={5}>
             <Stack>
-                {[...Array(40).keys()].map(() => <Skeleton height="40px"/>)}
+                {skeletonEl.map(() => <Skeleton height="40px"/>)}
             </Stack>
         </Container>
     )
